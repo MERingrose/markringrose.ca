712ddd953063265c67e7755021225778bc12e2c1
MERingrose <33042038+MERingrose@users.noreply.github.com>
2023-10-10 11:15:53 -0600

    added name as SVG file

--- src/app/page-Marquito-Machina.tsx
+++ src/app/page-Marquito-Machina.tsx
@@ -5,12 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <Image src="/images/mark_ringrose.svg"
-          width={500}
-          height={50}
-          alt='Mark Ringrose'
-          />
-        {/* <h1 className="text-center justify-between text-3xl font-bold ">MARK RINGROSE</h1> */}
+        <h1 className="text-center justify-between">Hello! Welcome to Mark&apos;s World.</h1>
         {/* <Image
           className='justify-center'
           src="/images/rose_logo.jpg"
@@ -23,7 +18,7 @@ export default function Home() {
           width={500}
           height={500}
           alt="Meditating Humanoid"
-          className='stroke-blue-500 stroke-2'
+          className=''
         />
         <Link href='/users'>Users</Link>
       </div>
