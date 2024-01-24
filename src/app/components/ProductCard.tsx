df30465cc24b18edae9a580153457a19b028356c
Mark Ringrose <mark.e.ringrose@gmail.com>
2024-01-23 17:06:10 -0700

    Changed styling for About and Works sections. Created custom component for ProductCard.

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { PropsWithChildren } from 'react'
+import Image from 'next/image'
 
-const ProductCard = () => {
+
+
+function ProductCard(props : {siteName : String, imgSrc : string, height : number, width : number, alt : string, link : string, description: string}){
     return (
-        <div>ProductCard</div>
+        <div className='inline-grid max-w-sm m-10  '>
+            <a href={props.link}>
+                <h1 className=''>{props.siteName}</h1>
+                <Image 
+                src={props.imgSrc}
+                height={props.height}
+                width={props.width}
+                alt={props.alt}
+                />
+                <h2 className=' text-wrap '>{props.description}</h2>
+            </a>
+        </div>
     )
 }
 
-export default ProductCard
+export default ProductCard;
--- src/app/page-Ringrose-Desktop.tsx
+++ src/app/page-Ringrose-Desktop.tsx
@@ -13,22 +13,16 @@ export default async function Home() {
   const key = "gWno1afgUHLXw2JN85O6QA==5jDaKRdeo28FGwBH";
 
   const quote = await fetch("https://api.api-ninjas.com/v1/quotes", {"headers" : { "X-Api-Key" : key}}).then(function(response) {
+    
     return response.json();
   }).then((data)=>{return data});
 
   console.log(quote);
 
    return (
-    <main className="flex flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       
-      <div id="section1" className='min-h-screen '>
-        <Image
-        src="/images/rose_logo.svg"
-        width={200}
-        height={200}
-        alt="A stylized ring and rose"
-        className='flex animate-[floating_7s_ease-in-out_infinite] bg-black'
-        />
+      <div>
         <Image
           src="/images/meditation.jpg"
           width={500}
@@ -38,8 +32,8 @@ export default async function Home() {
         />
         {/* <Link href='/users'>Users</Link> */}
       </div>
-      <div id="section2" className='flex flex-col items-center justify-between p-24'>
-        <h2 className=' mt-40px'>&quot;{quote[0].quote}&quot;</h2>
+      <div id="section2" className='min-h-screen back'>
+        <h2 className=' mt-40px'>"{quote[0].quote}"</h2>
         <h3 className='italic '>-{quote[0].author}</h3>
         
       </div>
