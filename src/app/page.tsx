44ecac638bda1322417298abf3d0ad6369dff7e8
Mark Ringrose <mark.e.ringrose@gmail.com>
2024-06-04 16:35:25 -0600

    put form framework in place on contact page to remove email address

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,6 @@ import Quote from './components/Quote';
 
 export default function Home(){
 
-
-
    return (
     <main className="flex flex-col items-center justify-between p-20">
       
@@ -21,10 +19,22 @@ export default function Home(){
           alt="Meditating Humanoid"
           className='stroke-blue-500 stroke-2 animate-[floating_5s_ease-in-out_infinite] '
         />
+
       </div>
+
+
       <div id="section2" className='flex flex-col items-center justify-between mt-5'>
-      <Quote />
+
+        <Quote />
+        
       </div>
+
+      <div className=' min-h-200 bg-slate-100 '>
+
+        {/* lighter section */}
+
+      </div>
+      
     </main>
   )
 }
